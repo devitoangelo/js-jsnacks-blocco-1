a2806a29a91ad05e819cca498e124bb5418e488a
devitoangelo <angelodevito4@gmail.com>
2024-02-23 12:44:36 +0100

    Snack"2

---
+++ Snack 2/main.js
@@ -0,0 +1,12 @@
+const parola1 = (prompt("Inserisci una parola"))
+const parola2 = (prompt("Inserisci una seconda parola"))
+
+
+if (parola1.length > parola2.length) {
+    console.log("la prima parola è piu lunga");
+
+} else if(parola1.length < parola2.length) {
+    console.log("la seconda parola è piu lunga");
+
+
+}
