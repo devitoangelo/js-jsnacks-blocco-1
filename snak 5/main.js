fdf7d21892c4ac81cddc5e94a603ba1d13d58c31
devitoangelo <angelodevito4@gmail.com>
2024-02-26 11:01:01 +0100

    snack 5

---
+++ snak 5/main.js
@@ -0,0 +1,20 @@
+//ciclo while
+let i = 0;
+
+while (i < 10) {
+
+    let  nomeUtente = prompt("inserisci il tuo nome")
+    console.log(nomeUtente);
+
+    i++
+
+}
+
+// ciclo for
+for (let i = 0; i < Utentenome.length; i++) {
+    const tv = Utentenome[i];
+
+    console.log(tv);
+
+}
+
